82c0b15f5244f27a29001fa7e14d4924ba5774f0
mariusaee <mariusae@me.com>
2021-12-06 11:58:16 +0300

    Initial Commit

---
+++ GestureState/GestureStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureStateApp.swift
+//  GestureState
+//
+//  Created by Marius Malyshev on 06.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct GestureStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
