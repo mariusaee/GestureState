3adfe105c8a327e3c895a5a3f026835fd0293fc2
mariusaee <mariusae@me.com>
2021-12-06 15:43:51 +0300

    @State added

--- GestureState/ContentView.swift
+++ GestureState/ContentView.swift
@@ -11,12 +11,14 @@ struct ContentView: View {
     
     @GestureState private var dragAmount = CGSize.zero
     @GestureState private var isLongPressed = false
+    @State private var position = CGSize.zero
     
     var body: some View {
         VStack(spacing: 30) {
             Image(systemName: "circle.fill")
                 .resizable()
                 .frame(width: 150, height: 150)
+                .foregroundColor(.red)
                 .offset(dragAmount)
                 .gesture(
                     DragGesture().updating($dragAmount) { value, state, transaction in
@@ -31,14 +33,27 @@ struct ContentView: View {
                 .foregroundColor(isLongPressed ? .red : .blue)
                 .scaleEffect(isLongPressed ? 2 : 1)
                 .gesture(
-                    LongPressGesture().updating($isLongPressed, body: { value, state, transaction in
+                    LongPressGesture().updating($isLongPressed) { value, state, transaction in
                         state = value
-                    })
+                    }
                 )
                 .animation(.spring(), value: isLongPressed)
+            
             Image(systemName: "square.fill")
                 .resizable()
                 .frame(width: 150, height: 150)
+                .foregroundColor(.red)
+                .offset(x: position.width, y: position.height)
+                .gesture(
+                    DragGesture()
+                        .onChanged({ value in
+                            position = value.translation
+                        })
+                        .onEnded({ value in
+                            position = .zero
+                        })
+                )
+                .animation(Animation.linear, value: position)
         }
     }
 }
