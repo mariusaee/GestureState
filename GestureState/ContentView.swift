62bdbf5528fb7b9e5035af32c3376e1433e18356
mariusaee <mariusae@me.com>
2021-12-06 12:07:11 +0300

    simple drag

--- GestureState/ContentView.swift
+++ GestureState/ContentView.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @GestureState var dragAmount = CGSize.zero
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Image(systemName: "star")
+            .offset(dragAmount)
+            .gesture(
+                DragGesture().updating($dragAmount) { value, state, transaction in
+                    state = value.translation
+                }
+            )
     }
 }
 
